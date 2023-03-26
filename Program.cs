e02ea1f0c14967cbb0cd7890eeb99b1d44d33ddf
raimess11 <rahma.sakti24@gmail.com>
2023-03-27 00:54:25 +0700

    implementasi design by contract

--- Program.cs
+++ Program.cs
@@ -6,5 +6,11 @@ internal class Program
     {
         SayaTubeVideo meTube = new SayaTubeVideo("Tutorial Design By Contract - [RAHMA SAKTI R]");
         meTube.PrintVideoDetails();
+
+        //SayaTubeVideo meTube1 = new SayaTubeVideo(null);
+        SayaTubeVideo meTube2 = new SayaTubeVideo(new string(new char[100]));
+        meTube.IncreasePlayCount(2);
+        meTube.IncreasePlayCount(int.MaxValue);
+        meTube.PrintVideoDetails();
     }
 }
--- SayaTubeVideo.cs
+++ SayaTubeVideo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,15 +14,31 @@ namespace tpmodul6_1302210095
         int playCount;
 
         Random random = new Random(DateTime.Now.Second);
-
         public SayaTubeVideo(string title)
         {
+            Debug.Assert(
+                title != null,
+                "title tidak boleh null"
+                );
+            Debug.Assert(
+                title.Length < 100,
+                "maksima karakter pada title adalah 100 karakter"
+                );
+
             this.title = title;
             this.id = random.Next(10000,99999);
             int playCount = 0;
         }
         public void IncreasePlayCount(int Count)
         {
+            Debug.Assert(
+                Count <= 10000000,
+                "batas penambahan play count adalah 10.000.000"
+                );
+
+            try{checked{playCount += Count;}}
+            catch( OverflowException e ){Console.WriteLine(e.Message);}
+
             this.playCount += Count;
         }
         public void PrintVideoDetails()
