723149d6a56bb259ec20bbf1bcf07b72c91b3167
raimess11 <rahma.sakti24@gmail.com>
2023-03-26 23:47:27 +0700

    menambahkan class SayaTubeVideo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul6_1302210095;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        SayaTubeVideo meTube = new SayaTubeVideo("Tutorial Design By Contract - [RAHMA SAKTI R]");
+        meTube.PrintVideoDetails();
+    }
+}
---
+++ SayaTubeVideo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_1302210095
+{
+    internal class SayaTubeVideo
+    {
+        int id;
+        string title;
+        int playCount;
+
+        Random random = new Random(DateTime.Now.Second);
+
+        public SayaTubeVideo(string title)
+        {
+            this.title = title;
+            this.id = random.Next(10000,99999);
+            int playCount = 0;
+        }
+        public void IncreasePlayCount(int Count)
+        {
+            this.playCount += Count;
+        }
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine(
+                    this.title + "\n#" + this.id + 
+                    "\ndiputar: " + this.playCount
+                );
+        }
+    }
+}
