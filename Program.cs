93d5d310f55b36f479444c74d31f8db086476151
raimess11 <rahma.sakti24@gmail.com>
2023-03-27 01:38:15 +0700

    implementasi design by contract part 2

--- Program.cs
+++ Program.cs
@@ -7,10 +7,14 @@ internal class Program
         SayaTubeVideo meTube = new SayaTubeVideo("Tutorial Design By Contract - [RAHMA SAKTI R]");
         meTube.PrintVideoDetails();
 
-        //SayaTubeVideo meTube1 = new SayaTubeVideo(null);
-        SayaTubeVideo meTube2 = new SayaTubeVideo(new string(new char[100]));
-        meTube.IncreasePlayCount(2);
-        meTube.IncreasePlayCount(int.MaxValue);
+        try { SayaTubeVideo meTube1 = new SayaTubeVideo(null); }
+        catch { Console.WriteLine("judul lagu tidak boleh null"); }
+        try { SayaTubeVideo meTube2 = new SayaTubeVideo(new string(new char[100] + "a")); }
+        catch { Console.WriteLine("karakter maksimum pada judul adalah 100 karakter"); }
+        try { meTube.IncreasePlayCount(3); }
+        catch { Console.WriteLine("batas penambahan play count adalah 10.000.000"); }
+        try { meTube.IncreasePlayCount(int.MaxValue); }
+        catch { Console.WriteLine("batas penambahan play count adalah 10.000.000"); }
         meTube.PrintVideoDetails();
     }
 }
